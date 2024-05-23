49cc85a991c3508f3b32c15c798868562e69fb0c
Oportunitas <taibizzat165@gmail.com>
2024-05-23 20:55:57 +0700

    chore: testing subtree pushing

--- page.tsx
+++ page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function page() {
     return (
         <div>
-            Nonogram Solver
+            This page is under development
         </div>
     )
 }
